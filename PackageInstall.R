1f32bd7662634767f1013aea44c1df258e20eb3f
klaudiapop04 <157442760+klaudiapop04@users.noreply.github.com>
2024-05-02 15:30:26 -0600

    Added files
    R wasn't letting me push, trying this way

---
+++ PackageInstall.R
@@ -0,0 +1,28 @@
+#This file is to install all necessary packages in the case of using a new 
+#computer
+
+#remember to use library() to open packages that need to be used
+
+install.packages('gapminder')
+install.packages("swirl")
+install.packages("tidyverse")
+install.packages("BiocManager")
+BiocManager::install("biomaRt")
+BiocManager::install("ggtree")
+install.packages("ggplot2")
+install.packages("RColorBrewer")
+install.packages("ggthemes")
+install.packages("ggpubr")
+install.packages("datasets")
+install.packages("scales")
+install.packages("readxl")
+install.packages("DBI")
+install.packages("RSQLite")
+install.packages("XML")
+install.packages("xml2")
+install.packages("rjson")
+install.packages("data.table")
+install.packages("rvest")
+install.packages("ShortRead")
+install.packages("devtools")
+#how to install qiime2R
